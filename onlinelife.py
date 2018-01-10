89fe33ebd835f90ceb0949c2ae8e2f3ecc55461a
Sergey Rodin <rodin.s@rambler.ru>
2018-01-10 16:42:36 +0200

    Add html to string function

--- onlinelife.py
+++ onlinelife.py
@@ -1,2 +1,18 @@
+import urllib2
+
+DOMAIN = "http://online-life.club"
 
 print("Online-life")
+
+def httpToString(url):
+    response = urllib2.urlopen(DOMAIN)
+    html = response.read();
+    return html
+
+page = httpToString(DOMAIN)
+print(page)
+
+#encoding = response.headers.get_content_charset('utf-8')
+#decoded_html = html_response.decode(encoding)
+#print(decoded_html)
+
